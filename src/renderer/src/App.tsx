9df23e618623f1fca145dc72f7500e0d3c241579
Sam Hamer <samuel.hamer@outlook.com>
2025-03-08 23:40:50 -0600

    init

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,9 @@
+function App(): JSX.Element {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-800">
+      <div className="text-3xl font-bold text-white">GSPro Connector</div>
+    </div>
+  )
+}
+
+export default App
