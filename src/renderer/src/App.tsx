ec0b9cce468eb34f266e4018585449a83be643c7
Sam Hamer <samuel.hamer@outlook.com>
2025-03-09 12:50:53 -0500

    working tailwind with shadcn components

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from './components/ui/button'
+
 function App(): JSX.Element {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-800">
-      <div className="text-3xl font-bold text-white">GSPro Connector</div>
+    <div className="flex flex-col items-center justify-center h-screen bg-background text-foreground">
+      <div className="text-3xl font-bold">GSPro Connector</div>
+      <Button variant="success" className="cursor-pointer">
+        Click me
+      </Button>
     </div>
   )
 }
